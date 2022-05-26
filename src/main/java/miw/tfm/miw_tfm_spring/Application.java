dd684f7fea4ffb901678e02813599ef4d6d775c8
pierorodriguez09 <33253562+prodrang@users.noreply.github.com>
2022-05-26 18:51:03 +0200

    Fixed functionality TFM-30

--- src/main/java/miw/tfm/miw_tfm_spring/Application.java
+++ src/main/java/miw/tfm/miw_tfm_spring/Application.java
@@ -1,9 +1,19 @@
 package miw.tfm.miw_tfm_spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"<base package name>"})
+@EnableJpaRepositories(basePackages = {"<repository package name>"})
+@EnableTransactionManagement
+@EntityScan(basePackages = "<entity package name>")
 public class Application {
 
 	public static void main(String[] args) {
