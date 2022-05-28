7209b447f2bcc366310405d333a2591484decae3
sergiomerayo <sergiomerayo@gmail.com>
2022-05-28 11:42:58 +0200

    TFM-35 fixed errors

--- src/main/java/miw/tfm/miw_tfm_spring/Application.java
+++ src/main/java/miw/tfm/miw_tfm_spring/Application.java
@@ -3,6 +3,9 @@ package miw.tfm.miw_tfm_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
@@ -11,4 +14,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
+
+
+
